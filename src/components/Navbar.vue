cee076113ba2326fb22946f51ddfc9df96e8ff86
Rafael Luciano <rafaeldasilva.luciano@gmail.com>
2024-10-19 20:42:27 -0300

    Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,38 +3,31 @@
       <router-link to="/HomeView" class="homeButton">&lt;Home&gt;</router-link>
       <router-link to="/AboutMeView" class="aboutMeButton">&lt;Sobre mim&gt;</router-link>
       <router-link to="/ProjectsView" class="projectsButton">&lt;Projetos&gt;</router-link>
-      <img src="../assets/config.svg" alt="Botão de configuração" class="configButton" @click="toggleDropdown">
-      <div v-if="isDropdownVisible" class="dropdown-menu">
-        <ul>
-          <li>Língua: Português</li>
-          <li>Modo: Light</li>
-        </ul>
-    </div>
+      <div class="configButton"><ConfigHomeView/></div>
     </div>
   </template>
   
   <script lang="ts">
-  import { ref } from 'vue';
 
-  export default {
+  import ConfigHomeView from '../views/ConfigHomeView.vue';
+
+  export default {      
       name: "Navbar",
-      setup() {
-    const isDropdownVisible = ref(false);
+      view: {
+        ConfigHomeView,
+      }
+}             
 
-    function toggleDropdown() {
-      isDropdownVisible.value = !isDropdownVisible.value;
-    }
-
-    return {
-      isDropdownVisible,
-      toggleDropdown
-    };
-  }
-}
-
-  </script>
+  </script>       
   
   <style>
+
+  .configButton {
+    background-color: blueviolet;
+  width: 200px;
+  height: 200px;
+}
+
   .navbar {
     display: flex;
     justify-content: space-between;
@@ -63,43 +56,6 @@
     font-size: 20px;
     font-weight: bold;
   }
-  
-  .configButton {
-    width: 50px;
-    height: 50px;
-    padding-right: 20px;
-  }
-
-  .menu-icon {
-  cursor: pointer;
-  width: 30px; 
-}
-
-.dropdown-menu {
-  position: absolute;
-  top: 100%; 
-  left: 0; 
-  background-color: white;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-  width: 100%; 
-}
-
-.dropdown-menu ul {
-  list-style: none;
-  padding: 0;
-  margin: 0; 
-}
-
-.dropdown-menu li {
-  padding: 8px 16px;
-  border-bottom: 1px solid #ccc;
-  cursor: pointer;
-}
-
-.dropdown-menu li:last-child {
-  border-bottom: none;
-}
 
 @media screen and (max-width: 1020px) {
     
@@ -107,11 +63,8 @@
     font-size: 15px;
     }
 
-    .configButton {
-    width: 30px;
-    height: 30px;
-    padding-right: 20px;
-  }
+
+
 }
   </style>
   
---
+++ src/views/ConfigHomeView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="div">
+    <img src="../assets/config.svg" alt="Botão de configuração" class="configButton">
+          <div v-if="isDropdownVisible" class="dropdown-menu">
+            <ul>
+              <li>Língua: Português</li>
+              <li>Modo: Light</li>
+            </ul>
+        </div>
+  </div>  
+</template>
+
+<script lang="ts">
+
+import { ref } from 'vue';
+
+export default {
+    name: "ConfigHomeView",
+    setup() {
+    const isDropdownVisible = ref(false);
+
+    function toggleDropdown() {
+      isDropdownVisible.value = !isDropdownVisible.value;
+    }
+
+    return {
+      isDropdownVisible,
+      toggleDropdown
+    };
+  }
+}
+</script>
+
+<style>
+
+.div {
+  background-color: blueviolet;
+  width: 200px;
+  height: 200px;
+}
+
+.configButton {
+    width: 50px;
+    height: 50px;
+    padding-right: 20px;
+  }
+
+  .menu-icon {
+  cursor: pointer;
+  width: 30px; 
+}
+
+/*Config do menu abaixo*/
+
+.dropdown-menu {
+  position: absolute;
+  top: 100%; 
+  right: 0; 
+  background-color: white;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  width: 15%; 
+}
+
+.dropdown-menu ul {
+  list-style: none;
+  padding: 0;
+  margin: 0; 
+}
+
+.dropdown-menu li {
+  padding: 8px 16px;
+  border-bottom: 1px solid #ccc;
+  cursor: pointer;
+}
+
+.dropdown-menu li:last-child {
+  border-bottom: none;
+}
+
+@media screen and (max-width: 1020px) {
+    
+    
+  }
+
+</style>
